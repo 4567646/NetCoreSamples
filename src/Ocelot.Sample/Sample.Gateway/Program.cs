1928b4c0965228060f72c93b51e8c5e8faeffbef
liaofeng <422139987@qq.com>
2019-04-10 23:03:14 +0800

    增加Ocelot

--- src/Ocelot.Sample/Sample.Gateway/Program.cs
+++ src/Ocelot.Sample/Sample.Gateway/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Sample.Gateway
 {
@@ -19,6 +13,11 @@ namespace Sample.Gateway
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureAppConfiguration((contex, builder) =>
+                {
+                    builder.SetBasePath(contex.HostingEnvironment.ContentRootPath).AddJsonFile("Ocelot.json");
+                })
+            .UseUrls("http://localhost:5000")
+            .UseStartup<Startup>();
     }
 }
