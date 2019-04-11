765f3433f77fa319e8e9f09a5c1e8a307e8712aa
lf <422139987@qq.com>
2019-04-11 11:35:57 +0800

    新增Ocelot分支

---
+++ src/Ocelot.Sample/Sample.ApiServiceB/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample.ApiServiceB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return HttpContext.Request.Host.Port + "" + _configuration["AppName"] + "" + DateTime.Now.ToString();
+        }
+
+
+    }
+}
