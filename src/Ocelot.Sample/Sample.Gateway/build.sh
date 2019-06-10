fc81aa6563f7f772927232246c39a10bf8586510
liaofeng <422139987@qq.com>
2019-06-10 21:20:48 +0800

    11111

--- src/Ocelot.Sample/Sample.Gateway/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker stop api.gateway
-sudo docker rm api.gateway
-
-sudo docker build -t api.gateway .
-sudo docker run --name=api.gateway -d -p 5000:5000 -e "ASPNETCORE_URLS=http://+:5000" api.gateway
